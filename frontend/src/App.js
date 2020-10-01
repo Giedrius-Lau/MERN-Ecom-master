6d33dfaa0f0e6173e65fcf41fb31ae17d3c7bd2e
Giedrius <lauruska.giedrius@gmail.com>
2020-10-01 21:19:28 +0300

    Added header and footer. Added dummy products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
-      <div className='App'>Hi</div>
+      <Header></Header>
+      <main className='py-3'>
+        <Container>
+          <div className='App'>Hi</div>
+        </Container>
+      </main>
+      <Footer></Footer>
     </>
   );
 };
