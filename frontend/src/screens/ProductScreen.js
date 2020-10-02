442a27eef012ed429bc4ff5e17515c50d545b28c
Giedrius <lauruska.giedrius@gmail.com>
2020-10-02 20:49:53 +0300

    Filled product page

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,8 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import products from '../products';
+import Rating from '../components/rating/Rating';
 
-const ProductScreen = (props) => {
-  return <div>Product</div>;
+const ProductScreen = ({ match }) => {
+  const product = products.find((p) => p._id === match.params.id);
+  return (
+    <>
+      <Link className='btn btn-dark my-3' to='/'>
+        Go back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid></Image>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating value={product.rating} text={` ${product.numReviews} reviews`}></Rating>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h4>${product.price}</h4>
+            </ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <ListGroup>
+            <ListGroup.Item>
+              <Row>
+                <Col>Price: </Col>
+              </Row>
+              <Row>
+                <Col>{product.price}</Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Col>Status: </Col>
+              </Row>
+              <Row>
+                <Col>{product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}</Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
+                Add To Cart
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 ProductScreen.propTypes = {};
