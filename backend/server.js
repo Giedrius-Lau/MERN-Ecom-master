b3174ff649a8ff3785d138cc4d2a97bcec6be782
Giedrius <lauruska.giedrius@gmail.com>
2020-10-04 13:29:48 +0300

    Connected mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const products = require('./data/products');
-const dotenv = require('dotenv');
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import products from './data/products.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 app.get('/api/products', (req, res) => {
