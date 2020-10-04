212c129af2bfbb0b7a983645bb0c437c4b75ca6b
Giedrius <lauruska.giedrius@gmail.com>
2020-10-04 18:34:47 +0300

    Added colors for server console logs

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import products from './data/products.js';
+import colors from 'colors';
 
 dotenv.config();
 
@@ -20,4 +21,4 @@ app.get('/api/products/:id', (req, res) => {
 const PORT = process.env.PORT || 5000;
 const ENVIROMENT = process.env.NODE_ENV;
 
-app.listen(PORT, console.log(`Server running in [${ENVIROMENT}] mode on port ${PORT}`));
+app.listen(PORT, console.log(`Server running in [${ENVIROMENT}] mode on port ${PORT}`.yellow.bold));
