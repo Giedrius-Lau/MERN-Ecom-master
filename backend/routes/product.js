720567ecee98801c655c5b5c1af7e79856fed294
Giedrius <lauruska.giedrius@gmail.com>
2020-10-06 20:47:01 +0300

    Added product route

---
+++ backend/routes/product.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+import products from '../data/products.js';
+import Product from '../models/Product.js';
+const router = express.Router();
+
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
+
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: 'Product not found' });
+    }
+  })
+);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import products from './data/products.js';
 import colors from 'colors';
+import product from './routes/product.js';
 
 dotenv.config();
 
 connectDB();
-
 const app = express();
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', product);
 
 const PORT = process.env.PORT || 5000;
 const ENVIROMENT = process.env.NODE_ENV;
