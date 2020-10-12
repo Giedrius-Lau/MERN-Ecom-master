8fee8013d6679b56690d2d8b0538adc51fcd1a95
Giedrius <lauruska.giedrius@gmail.com>
2020-10-12 20:43:52 +0300

    Initialised redux in cartscreen, removed bugs

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,15 +2,17 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
+import { cartReducer } from './reducers/cartReducers';
 import { productListReducer, productDetailsReducer } from './reducers/productListReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
-
+console.log(cartItemsFromStorage);
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
 };
