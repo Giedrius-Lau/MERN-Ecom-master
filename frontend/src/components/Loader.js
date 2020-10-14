2856d5622b87a4db42575ca428ad79c22047948a
Giedrius <lauruska.giedrius@gmail.com>
2020-10-14 12:06:23 +0300

    Changed Loader

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import { Spinner } from 'react-bootstrap';
 
 const Loader = () => {
   return (
-    <Spinner
-      animation='border'
-      role='status'
-      style={{
-        width: '100px',
-        height: '100px',
-        margin: 'auto',
-        display: 'block',
-      }}
-    >
-      <span className='sr-only'>Loading...</span>
-    </Spinner>
+    <div class='sk-chase'>
+      <div class='sk-chase-dot'></div>
+      <div class='sk-chase-dot'></div>
+      <div class='sk-chase-dot'></div>
+      <div class='sk-chase-dot'></div>
+      <div class='sk-chase-dot'></div>
+      <div class='sk-chase-dot'></div>
+    </div>
   );
 };
 
