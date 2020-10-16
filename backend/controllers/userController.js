1c7f7521643d82afff11cf677cfeda7f06990ee0
Giedrius <lauruska.giedrius@gmail.com>
2020-10-16 16:17:22 +0300

    Added token util

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,19 +1,19 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/UserModel.js';
+import generateToken from '../utils/generateToken.js';
 
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
 
-  console.log(await user.matchPassword(password)))
   if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
