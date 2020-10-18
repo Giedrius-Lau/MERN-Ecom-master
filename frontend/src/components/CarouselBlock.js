e1755d273eca9915f0025eaeb479ceed521400d0
Giedrius <lauruska.giedrius@gmail.com>
2020-10-18 22:24:07 +0300

    Style changes

--- frontend/src/components/CarouselBlock.js
+++ frontend/src/components/CarouselBlock.js
@@ -1,34 +1,40 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Button } from 'react-bootstrap';
+import Fade from 'react-reveal/Fade';
 
 const CarouselBlock = () => {
   return (
     <>
-      <Carousel controls='false' interval='30000'>
-        <Carousel.Item>
-          <img className='d-block w-100' src='/images/1.jpg' alt='First slide' />
-          <Carousel.Caption>
-            <h3>Camera</h3>
-            <p>Camera slide.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className='d-block w-100' src='/images/3.jpg' alt='Second slide' />
+      <Fade duration={1000}>
+        <Carousel controls='false' interval='30000'>
+          <Carousel.Item>
+            <img className='d-block w-100' src='/images/1.jpg' alt='First slide' />
+            <Carousel.Caption>
+              <h3>Camera</h3>
+              <p>Camera slide.</p>
+              <Button variant='primary'>Shop</Button>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className='d-block w-100' src='/images/3.jpg' alt='Second slide' />
 
-          <Carousel.Caption>
-            <h3>Apple</h3>
-            <p>Apple slide.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className='d-block w-100' src='/images/2.jpg' alt='Third slide' />
+            <Carousel.Caption>
+              <h3>Apple</h3>
+              <p>Apple slide.</p>
+              <Button variant='primary'>Shop</Button>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className='d-block w-100' src='/images/2.jpg' alt='Third slide' />
 
-          <Carousel.Caption>
-            <h3>Lens</h3>
-            <p>Lens slide.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+            <Carousel.Caption>
+              <h3>Lens</h3>
+              <p>Lens slide.</p>
+              <Button variant='primary'>Shop</Button>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Fade>
     </>
   );
 };
