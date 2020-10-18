a21434412b31396a085b1b4f3340c264f042bc11
Giedrius <lauruska.giedrius@gmail.com>
2020-10-18 16:11:41 +0300

    Added auth token middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,4 +21,22 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+
+  res.send('success');
+});
+
+export { authUser, getUserProfile };
