1c64348912be581d9c6476f6ea626758ab9533ff
Giedrius <lauruska.giedrius@gmail.com>
2020-10-23 11:30:32 +0300

    Updated seeder

--- backend/seeder.js
+++ backend/seeder.js
@@ -3,9 +3,9 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import users from './data/users.js';
 import products from './data/products.js';
-import User from './models/User.js';
-import Product from './models/Product.js';
-import Order from './models/Order.js';
+import UserModel from './models/UserModel.js';
+import ProductModel from './models/ProductModel.js';
+import OrderModel from './models/OrderModel.js';
 import connectDB from './config/db.js';
 
 dotenv.config();
@@ -13,45 +13,45 @@ dotenv.config();
 connectDB();
 
 const importData = async () => {
-  try {
-    await User.deleteMany();
-    await Product.deleteMany();
-    await Order.deleteMany();
+    try {
+        await UserModel.deleteMany();
+        await ProductModel.deleteMany();
+        await OrderModel.deleteMany();
 
-    const createdUsers = await User.insertMany(users);
+        const createdUsers = await UserModel.insertMany(users);
 
-    const adminUser = createdUsers[0]._id;
+        const adminUser = createdUsers[0]._id;
 
-    const sampleProducts = products.map((product) => {
-      return { ...product, user: adminUser };
-    });
+        const sampleProducts = products.map((product) => {
+            return { ...product, user: adminUser };
+        });
 
-    await Product.insertMany(sampleProducts);
+        await ProductModel.insertMany(sampleProducts);
 
-    console.log('Data Imported!'.green.inverse);
-    process.exit();
-  } catch (error) {
-    console.error(`${error}`.red.inverse);
-    process.exit(1);
-  }
+        console.log('Data Imported!'.green.inverse);
+        process.exit();
+    } catch (error) {
+        console.error(`${error}`.red.inverse);
+        process.exit(1);
+    }
 };
 
 const desctroyData = async () => {
-  try {
-    await User.deleteMany();
-    await Product.deleteMany();
-    await Order.deleteMany();
+    try {
+        await UserModel.deleteMany();
+        await ProductModel.deleteMany();
+        await OrderModel.deleteMany();
 
-    console.log('All data erased!'.red.inverse);
-    process.exit();
-  } catch (error) {
-    console.error(`${error}`.red.inverse);
-    process.exit(1);
-  }
+        console.log('All data erased!'.red.inverse);
+        process.exit();
+    } catch (error) {
+        console.error(`${error}`.red.inverse);
+        process.exit(1);
+    }
 };
 
 if (process.argv[2] === '-d') {
-  desctroyData();
+    desctroyData();
 } else {
-  importData();
+    importData();
 }
