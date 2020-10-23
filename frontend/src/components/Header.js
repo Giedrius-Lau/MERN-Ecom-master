a43cbc7187724d1018e3d3c75c7ca5171e8f5e25
Giedrius <lauruska.giedrius@gmail.com>
2020-10-23 09:22:25 +0300

    added dark mode

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,34 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Container, Navbar, Nav } from 'react-bootstrap';
+import * as Icon from 'react-bootstrap-icons';
 
 const Header = () => {
-  return (
-    <header>
-      <Navbar fixed='top' expand='lg' collapseOnSelect>
-        <Container>
-          <LinkContainer to='/'>
-            <Navbar.Brand className='logo'>MERN Shop</Navbar.Brand>
-          </LinkContainer>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ml-auto'>
-              <LinkContainer to='/cart'>
-                <Nav.Link>
-                  <i className='fas fa-shopping-cart'></i> Cart
-                </Nav.Link>
-              </LinkContainer>
-              <LinkContainer to='/login'>
-                <Nav.Link>
-                  <i className='fas fa-user'></i> Sign In
-                </Nav.Link>
-              </LinkContainer>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
-  );
+    const [darkMode, setDarkMode] = useState(localStorage.getItem('theme') === 'dark');
+    let theme = localStorage.getItem('theme');
+
+    if (theme === 'dark') enableDarkMode();
+
+    const toggleDarkTheme = () => {
+        setDarkMode(!darkMode);
+        theme = localStorage.getItem('theme');
+        if (theme === 'dark') {
+            disableDarkMode();
+        } else {
+            enableDarkMode();
+        }
+    };
+
+    function enableDarkMode() {
+        localStorage.setItem('theme', 'dark');
+        document.body.classList.add('dark-mode');
+    }
+
+    function disableDarkMode() {
+        localStorage.setItem('theme', 'light');
+        document.body.classList.remove('dark-mode');
+    }
+
+    window.matchMedia('(prefers-color-scheme: dark)').addListener((e) => (e.matches ? enableDarkMode() : disableDarkMode()));
+    return (
+        <header>
+            <Navbar fixed='top' expand='lg' collapseOnSelect>
+                <Container>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand className='logo'>MERN Shop</Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                    <Navbar.Collapse id='basic-navbar-nav'>
+                        <Nav className='ml-auto'>
+                            <div className='dark-mode' onClick={() => toggleDarkTheme()}>
+                                {darkMode ? <Icon.Moon size={36}></Icon.Moon> : <Icon.Sun size={36}></Icon.Sun>}
+                            </div>
+                            <LinkContainer to='/login'>
+                                <Nav.Link>
+                                    <Icon.PersonFill size={36}></Icon.PersonFill>{' '}
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link>
+                                    <Icon.BagFill size={36}></Icon.BagFill>
+                                </Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
 };
 
 export default Header;
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -11,99 +11,107 @@ import Message from '../components/Message';
 import NarrowContainer from '../components/NarrowContainer';
 
 const ProductScreen = ({ history, match }) => {
-  const [qty, setQty] = useState(1);
-  const dispatch = useDispatch();
+    const [qty, setQty] = useState(1);
+    const dispatch = useDispatch();
 
-  const productDetails = useSelector((state) => state.productDetails);
-  const { loading, error, product } = productDetails;
+    const productDetails = useSelector((state) => state.productDetails);
+    const { loading, error, product } = productDetails;
 
-  useEffect(() => {
-    dispatch(listProductsDetails(match.params.id));
-  }, [dispatch, match]);
+    useEffect(() => {
+        dispatch(listProductsDetails(match.params.id));
+    }, [dispatch, match]);
 
-  const addToCartHandler = () => {
-    history.push(`/cart/${match.params.id}?qty=${qty}`);
-  };
+    const addToCartHandler = () => {
+        history.push(`/cart/${match.params.id}?qty=${qty}`);
+    };
 
-  return (
-    <>
-      <NarrowContainer>
-        <Link className='btn btn-dark my-3' to='/'>
-          Go back
-        </Link>
-        {loading ? (
-          <Loader></Loader>
-        ) : error ? (
-          <Message variant='danger'>{error}</Message>
-        ) : (
-          <Fade duration={300} cascade>
-            <Row>
-              <Col md={6}>
-                <Fade duration={1000}>
-                  <Image src={product.image} alt={product.name} fluid></Image>
-                </Fade>
-              </Col>
-              <Col md={3}>
-                <ListGroup variant='flush'>
-                  <ListGroup.Item>
-                    <h3>{product.name}</h3>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <Rating value={product.rating} text={` ${product.numReviews} reviews`}></Rating>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <h4>${product.price}</h4>
-                  </ListGroup.Item>
-                  <ListGroup.Item>Description: {product.description}</ListGroup.Item>
-                </ListGroup>
-              </Col>
-              <Col md={3}>
-                <ListGroup>
-                  <ListGroup.Item>
-                    <Row>
-                      <Col>Price: </Col>
-                    </Row>
-                    <Row>
-                      <Col>{product.price}</Col>
-                    </Row>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <Row>
-                      <Col>Status: </Col>
-                    </Row>
-                    <Row>
-                      <Col>{product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}</Col>
-                    </Row>
-                  </ListGroup.Item>
-                  {product.countInStock > 0 && (
-                    <ListGroup.Item>
-                      <Row>
-                        <Col>Qty</Col>
-                        <Col>
-                          <Form.Control as='select' value={qty} onChange={(e) => setQty(e.target.value)}>
-                            {[...Array(product.countInStock).keys()].map((x) => (
-                              <option key={x + 1} value={x + 1}>
-                                {x + 1}
-                              </option>
-                            ))}
-                          </Form.Control>
-                        </Col>
-                      </Row>
-                    </ListGroup.Item>
-                  )}
-                  <ListGroup.Item>
-                    <Button onClick={addToCartHandler} className='btn-block' type='button' disabled={product.countInStock === 0}>
-                      Add To Cart
-                    </Button>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Col>
-            </Row>
-          </Fade>
-        )}
-      </NarrowContainer>
-    </>
-  );
+    return (
+        <>
+            <NarrowContainer>
+                <Link className='btn btn-dark my-3' to='/'>
+                    Go back
+                </Link>
+                {loading ? (
+                    <Loader></Loader>
+                ) : error ? (
+                    <Message variant='danger'>{error}</Message>
+                ) : (
+                    <Fade duration={300} cascade>
+                        <Row>
+                            <Col md={8}>
+                                <Fade duration={1000}>
+                                    <Image src={product.image} alt={product.name} fluid></Image>
+                                </Fade>
+                            </Col>
+                            <Col md={4}>
+                                <ListGroup variant='flush'>
+                                    <ListGroup.Item>
+                                        <h3>{product.name}</h3>
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        <Rating value={product.rating} text={` ${product.numReviews} reviews`}></Rating>
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        <h4>${product.price}</h4>
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+                                </ListGroup>
+
+                                <ListGroup>
+                                    <ListGroup.Item>
+                                        <Row>
+                                            <Col>Price: </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col>{product.price}</Col>
+                                        </Row>
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        <Row>
+                                            <Col>Status: </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col>{product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}</Col>
+                                        </Row>
+                                    </ListGroup.Item>
+                                    {product.countInStock > 0 && (
+                                        <ListGroup.Item>
+                                            <Row>
+                                                <Col>Qty</Col>
+                                                <Col>
+                                                    <Form.Control
+                                                        as='select'
+                                                        value={qty}
+                                                        onChange={(e) => setQty(e.target.value)}
+                                                    >
+                                                        {[...Array(product.countInStock).keys()].map((x) => (
+                                                            <option key={x + 1} value={x + 1}>
+                                                                {x + 1}
+                                                            </option>
+                                                        ))}
+                                                    </Form.Control>
+                                                </Col>
+                                            </Row>
+                                        </ListGroup.Item>
+                                    )}
+                                    <ListGroup.Item>
+                                        <Button
+                                            onClick={addToCartHandler}
+                                            className='btn-block'
+                                            type='button'
+                                            disabled={product.countInStock === 0}
+                                        >
+                                            Add To Cart
+                                        </Button>
+                                    </ListGroup.Item>
+                                </ListGroup>
+                            </Col>
+                        </Row>
+                    </Fade>
+                )}
+            </NarrowContainer>
+        </>
+    );
 };
 
 export default ProductScreen;
