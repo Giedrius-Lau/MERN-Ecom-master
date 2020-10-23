450503441704b02919f0b0e8c8aa508e29f8fe57
Giedrius <lauruska.giedrius@gmail.com>
2020-10-23 13:55:14 +0300

    Non important commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import './bootstrap.min.css';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
 
 serviceWorker.unregister();
