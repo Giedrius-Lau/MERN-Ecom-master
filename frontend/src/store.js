380a410582557f624c64d9cf996fffd34533f2df
Giedrius <lauruska.giedrius@gmail.com>
2020-10-24 11:06:55 +0300

    Added user actions

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,8 +14,10 @@ const reducer = combineReducers({
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null;
 const initialState = {
     cart: { cartItems: cartItemsFromStorage },
+    userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
