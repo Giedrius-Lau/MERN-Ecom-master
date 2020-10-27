758fab8650e3f3a31e080c1a2d0396497876e1dd
Giedrius <lauruska.giedrius@gmail.com>
2020-10-27 20:25:13 +0200

    Bug fix

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -64,6 +64,8 @@ export const userUpdateProfileReducer = (state = { user: {} }, action) => {
             return { loading: false, success: true, userInfo: action.payload };
         case USER_UPDATE_PROFILE_FAIL:
             return { loading: false, error: action.payload };
+        case USER_UPDATE_PROFILE_RESET:
+            return {};
         default:
             return state;
     }
