c6f7dfcc10c8897c94e59125e5b386b4eb8c330e
Giedrius <lauruska.giedrius@gmail.com>
2020-10-28 15:40:36 +0200

    Added shipping page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 const App = () => {
     return (
@@ -26,6 +27,7 @@ const App = () => {
                     <Route path='/products' component={ProductListScreen} />
                     <Route path='/product/:id' component={ProductScreen} />
                     <Route path='/cart/:id?' component={CartScreen}></Route>
+                    <Route path='/shipping' component={ShippingScreen}></Route>
                     <Route path='/' component={HomeScreen} exact />
                 </Container>
             </main>
