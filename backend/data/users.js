21097ab268f9b243d6d3cbf21ac4912910cf6899
Giedrius <lauruska.giedrius@gmail.com>
2020-11-12 18:03:58 +0200

    .

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,22 +1,22 @@
 import bcrypt from 'bcryptjs';
 
 const users = [
-  {
-    name: 'Admin user',
-    email: 'admin@example.com',
-    password: bcrypt.hashSync('123456', 10),
-    isAdmin: true,
-  },
-  {
-    name: 'Tom Ridlis',
-    email: 'tom@example.com',
-    password: bcrypt.hashSync('123456', 10),
-  },
-  {
-    name: 'This That',
-    email: 'This@example.com',
-    password: bcrypt.hashSync('123456', 10),
-  },
+    {
+        name: 'Admin user',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true,
+    },
+    {
+        name: 'Tom Ridlis',
+        email: 'tom@example.com',
+        password: bcrypt.hashSync('123456', 10),
+    },
+    {
+        name: 'This That',
+        email: 'This@example.com',
+        password: bcrypt.hashSync('123456', 10),
+    },
 ];
 
 export default users;
--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -2,40 +2,40 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 
 const userSchema = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        isAdmin: {
+            type: String,
+            required: true,
+            default: false,
+        },
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    isAdmin: {
-      type: String,
-      required: true,
-      default: false,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
+    return await bcrypt.compare(enteredPassword, this.password);
 };
 
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    next();
-  }
+    if (!this.isModified('password')) {
+        next();
+    }
 
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 const User = mongoose.model('User', userSchema);
