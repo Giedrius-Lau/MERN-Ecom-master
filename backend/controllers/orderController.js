d70aac6c2c0307e094e08fe2c95e8130bf5345b6
Giedrius <lauruska.giedrius@gmail.com>
2020-12-17 14:22:03 +0200

    Added backend order paid

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -45,4 +45,25 @@ const getOrderById = asyncHandler(async (req, res) => {
     }
 });
 
-export { addOrderItems, getOrderById };
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+        res.json(order);
+        (order.isPaid = true), (order.paidAt = Date.now());
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address,
+        };
+
+        const updatedOrder = await order.save();
+        res.json(updatedOrder);
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
