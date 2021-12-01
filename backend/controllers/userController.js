bd59b68770f3a3344f6df54d1758789abeff4a2e
Giedrius Lauruška <lauruska.giedrius@gmail.com>
2021-12-01 08:33:01 +0200

    Delete user route and redux

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -102,4 +102,22 @@ const getUsers = asyncHandler(async (req, res) => {
     res.json(users);
 });
 
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers };
+const getUserById = asyncHandler(async (req, res) => {
+    const user = await User.find(req.params.id).select('-password');
+
+    res.json(user);
+});
+
+const deleteUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+
+    if (user) {
+        await user.remove();
+        res.json({ message: 'User removed' });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser, getUserById };
