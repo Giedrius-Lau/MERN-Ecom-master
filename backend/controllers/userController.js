5610fe5399a57486e3c672d4a6d1201fb8813c87
Giedrius Lauruška <lauruska.giedrius@gmail.com>
2021-12-01 08:39:00 +0200

    User update added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -105,7 +105,12 @@ const getUsers = asyncHandler(async (req, res) => {
 const getUserById = asyncHandler(async (req, res) => {
     const user = await User.find(req.params.id).select('-password');
 
-    res.json(user);
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 const deleteUser = asyncHandler(async (req, res) => {
@@ -120,4 +125,28 @@ const deleteUser = asyncHandler(async (req, res) => {
     }
 });
 
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser, getUserById };
+const updateUserByAdmin = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        user.isAdmin = req.body.isAdmin;
+
+        const updatedUser = await user.save();
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
+
+    res.send('success');
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser, getUserById, updateUserByAdmin };
